29223962549cb647e92d7045a42dc2fee59c596a
Max-Rohrer20 <maxrohrer2023@gmail.com>
2024-08-02 11:37:51 -0300

    DOCS: adição de novas páginas na landingpage

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -2,10 +2,15 @@
 function getYear() {
     var currentDate = new Date();
     var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
+    var yearElement = document.querySelector("#displayYear");
+    if (yearElement) {
+        yearElement.innerHTML = currentYear;
+    } else {
+        console.error("Element with ID 'displayYear' not found.");
+    }
 }
 
-getYear();
+document.addEventListener("DOMContentLoaded", getYear);
 
 /** google_map js **/
 
@@ -14,5 +19,17 @@ function myMap() {
         center: new google.maps.LatLng(40.712775, -74.005973),
         zoom: 18,
     };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var mapElement = document.getElementById("googleMap");
+    if (mapElement) {
+        var map = new google.maps.Map(mapElement, mapProp);
+    } else {
+        console.error("Element with ID 'googleMap' not found.");
+    }
 }
+
+document.querySelectorAll('.card').forEach(card => {
+    const bgColor = card.getAttribute('data-bg-color');
+    if (bgColor) {
+        card.style.setProperty('--bg-color', bgColor);
+    }
+});
